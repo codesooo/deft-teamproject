33a1037eb729419353fe846844772fc127a573c6
neulHa33 <moonn3940@gmail.com>
2023-01-18 18:42:01 +0900

    일정 post 추가

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,15 @@
 import Router from 'koa-router';
 import posts from './posts';
 import auth from './auth';
+import course from './course';
+import schedule from './schedule';
 
 const api = new Router();
 
 api.use('/posts', posts.routes());
+api.use('/course', course.routes());
 api.use('/auth', auth.routes());
+api.use('/schedule', schedule.routes());
 
 // 라우터를 내보냅니다.
 export default api;
