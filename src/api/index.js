7af13a974918fe289db64084e1de32d40909d3e4
Youmin Lee <babybana0829@naver.com>
2023-01-19 15:13:10 +0900

    회원정보 업로드

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import posts from './posts';
 import auth from './auth';
 import course from './course';
 import schedule from './schedule';
+import consumer from './consumer';
 
 const api = new Router();
 
@@ -10,6 +11,7 @@ api.use('/posts', posts.routes());
 api.use('/course', course.routes());
 api.use('/auth', auth.routes());
 api.use('/schedule', schedule.routes());
+api.use('/consumer', consumer.routes());
 
 // 라우터를 내보냅니다.
 export default api;
