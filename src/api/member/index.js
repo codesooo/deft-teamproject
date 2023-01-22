95753e0f2f82943737a6759431e216833b62e870
Youmin Lee <babybana0829@naver.com>
2023-01-23 00:56:34 +0900

    검색, 담당회원 조회 기능 추가

--- src/api/member/index.js
+++ src/api/member/index.js
@@ -11,6 +11,8 @@ member.get('/admin/:id', adminCtrl.read);
 member.delete('/admin/:id', adminCtrl.remove);
 member.patch('/admin/:id', adminCtrl.update);
 
+member.post('/admin/search', adminCtrl.search); // 관리자 검색
+
 // 코치
 member.get('/coach', coachCtrl.list);
 member.post('/coach/create', coachCtrl.coachCreate);
@@ -18,5 +20,11 @@ member.get('/coach/:id', coachCtrl.read);
 member.delete('/coach/:id', coachCtrl.remove);
 member.patch('/coach/:id', coachCtrl.update);
 member.post('coach/searchuser', coachCtrl.searchUsers);
+
+member.get('/coach/responsible/:manager', coachCtrl.responsible); // 담당회원 조회
+member.post('/coach/search', coachCtrl.search); // 코치 검색
+
+// 구성원 (관리자, 코치 통합)
+member.post('/search', coachCtrl.searchMember); // 관리자 검색 -> 코드는 coach.ctrl.js에 있음
 
 export default member;
